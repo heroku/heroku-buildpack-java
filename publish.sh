5587c5ef2134c5e86c27d68aa43c909e7b7037ee
Joe Kutner <jpkutner@gmail.com>
2014-08-14 16:28:26 -0500

    added a script to use for publishing new versions of the buildpack

---
+++ publish.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ ! -z "$1" ]; then
+  pushd . > /dev/null 2>&1
+  cd /tmp &&
+  git clone git@github.com:heroku/heroku-buildpack-java.git &&
+  cd heroku-buildpack-java &&
+  git checkout master &&
+  heroku buildpacks:publish $1/java
+  popd > /dev/null 2>&1
+  echo "Cleaning up..."
+  rm -rf /tmp/heroku-buildpack-java
+  echo "Done."
+else
+  echo "You must provide a buildkit organization as an argument!"
+  exit 1
+fi
