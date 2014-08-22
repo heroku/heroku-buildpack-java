d5e96bc3c7e5bd24c180e3f13d8a9a053b295173
Joe Kutner <jpkutner@gmail.com>
2014-08-22 10:25:56 -0500

    tweaked the travis setup

--- etc/hatchet_travis_setup.sh
+++ etc/hatchet_travis_setup.sh
@@ -7,14 +7,21 @@ Host github.com
     StrictHostKeyChecking no
 EOF
 
-if [ `git config --get user.email` ]; then
+if [ -n "`git config --get user.email`" ]; then
   echo 'already set'; else `git config --global user.email 'buildpack@example.com'`
 fi
-if [ `git config --get user.name` ]; then
+if [ -n "`git config --get user.name`" ]; then
   echo 'already set'; else `git config --global user.name 'BuildpackTester'`
 fi
 
-echo "$CONFIG_SSH" >> ~/.ssh/config
+cat <<EOF > ~/.ssh/config
+Host heroku.com
+    StrictHostKeyChecking no
+    CheckHostIP no
+    UserKnownHostsFile=/dev/null
+Host github.com
+    StrictHostKeyChecking no
+EOF
 
 curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
