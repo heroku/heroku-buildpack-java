8802ba128380c7534bb34bc72b073524221d4404
Joe Kutner <jpkutner@gmail.com>
2014-08-23 12:11:26 -0500

    added korvan tests

--- spec/java_spec.rb
+++ spec/java_spec.rb
@@ -10,10 +10,10 @@ describe "Java" do
     end
   end
 
-  let(:app) { Hatchet::Runner.new("java-servlets-sample") }
 
   ["1.7", "1.8"].each do |version|
     context "on jdk-#{version}" do
+      let(:app) { Hatchet::Runner.new("java-servlets-sample") }
       let(:jdk_version) { version }
       it "should not install settings.xml" do
         app.deploy do |app|
@@ -37,6 +37,38 @@ describe "Java" do
         expect(app).not_to be_deployed
         expect(app.output).to include("javac: invalid target release: 1.7")
         expect(app.output).to include("BUILD FAILURE")
+      end
+    end
+  end
+
+
+  context "korvan" do
+    ["1.6", "1.7"].each do |version|
+      let(:app) { Hatchet::Runner.new("korvan") }
+      context "on jdk-#{version}" do
+        let(:jdk_version) { version }
+        it "runs commands" do
+          app.deploy do |app|
+            expect(app).to be_deployed
+            expect(app.output).to include("Installing OpenJDK #{jdk_version}")
+            expect(app.output).to include("Installing Maven 3.0.3")
+            expect(app.output).not_to include("Installing settings.xml")
+            expect(app.output).not_to include("BUILD FAILURE")
+
+            expect(successful_body(app)).to eq("/1")
+
+            expect(app.run("jce")).
+                to include("Picked up JAVA_TOOL_OPTIONS:  -Djava.rmi.server.useCodebaseOnly=true -Djava.rmi.server.useCodebaseOnly=true").
+                and include(%q{Encrypting, "Test"}).
+                and include(%q{Decrypted: Test})
+
+            expect(app.run("netpatch")).
+                to include("Picked up JAVA_TOOL_OPTIONS:  -Djava.rmi.server.useCodebaseOnly=true -Djava.rmi.server.useCodebaseOnly=true").
+                and include(%q{name:eth0 (eth0)}).
+                and include(%q{name:lo (lo)})
+
+          end
+        end
       end
     end
   end
