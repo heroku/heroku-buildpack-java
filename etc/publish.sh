75203c93f4d9df76875f55bd42f16063b2e635c1
Joe Kutner <jpkutner@gmail.com>
2014-09-17 23:50:47 -0500

    improved publish script

--- etc/publish.sh
+++ etc/publish.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 if [ ! -z "$1" ]; then
+  rm -rf /tmp/heroku-buildpack-java
   pushd . > /dev/null 2>&1
   cd /tmp &&
   git clone git@github.com:heroku/heroku-buildpack-java.git &&
   cd heroku-buildpack-java &&
   git checkout master &&
-  find . -not -name 'bin' -maxdepth 1 -delete &&
+  find . ! -name '.' ! -name '..' ! -name 'bin' -maxdepth 1 -print0 | xargs -0 rm -rf --
   heroku buildpacks:publish $1/java
   popd > /dev/null 2>&1
   echo "Cleaning up..."
