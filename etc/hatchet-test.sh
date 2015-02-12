c9e9b459d4733447590c9b52fde8becc311dc9a0
Joe Kutner <jpkutner@gmail.com>
2015-02-12 14:34:58 -0600

    Removed custom jvm-common settings from hatchet tests

--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -z "$JVM_COMMON_BRANCH" ]; then
-  jvmCommonUrl="https://api.github.com/repos/heroku/heroku-buildpack-jvm-common/tarball/${JVM_COMMON_BRANCH}"
-fi
+#if [ -n "$JVM_COMMON_BRANCH" ]; then
+  #jvmCommonUrl="https://api.github.com/repos/heroku/heroku-buildpack-jvm-common/tarball/${JVM_COMMON_BRANCH}"
+#elif [ -n "$JVM_COMMON_BUILDKIT" ]; then
+  #jvmCommonUrl="https://codon-buildpacks.s3.amazonaws.com/buildpacks/$JVM_COMMON_BUILDKIT/jvm-common.tgz"
+#fi
 
 hatchet install &&
 HATCHET_RETRIES=3 \
 HATCHET_DEPLOY_STRATEGY=git \
 HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-java.git" \
 HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#') \
-JVM_COMMON_BUILDPACK="${JVM_COMMON_BUILDPACK:-jvmCommonUrl}" \
 rspec $@
