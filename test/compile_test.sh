a13dd02c35cec229dd4330a590ed4e04be11ab76
Joe Kutner <jpkutner@gmail.com>
2015-02-12 17:35:05 -0600

    Removed test for legacy removeM2Cache

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -74,7 +74,7 @@ _assertMaven305() {
   assertFileMD5 "7d2bdb60388da32ba499f953389207fe" ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
 
-  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -file.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
+  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -Dfile.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
   assertCaptured "BUILD SUCCESS"
 }
 
@@ -83,7 +83,7 @@ _assertMaven311() {
   assertFileMD5 "08a6e3ab11f4add00d421dfa57ef4c85" ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
 
-  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -file.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
+  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -Dfile.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
   assertCaptured "BUILD SUCCESS"
 }
 
@@ -92,7 +92,7 @@ _assertMavenLatest() {
   assertFileMD5 "9d4c6b79981a342940b9eff660070748" ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
 
-  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -file.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
+  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -Dfile.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
   assertCaptured "BUILD SUCCESS"
 }
 
@@ -155,28 +155,10 @@ testNewAppsRemoveM2Cache()
   rm -r ${CACHE_DIR} # simulate a brand new app without a cache dir
 
   assertFalse "Precondition: New apps should not have a CACHE_DIR prior to running" "[ -d ${CACHE_DIR} ]"
-  assertFalse "Precondition: New apps should not have a removeM2Cache file prior to running" "[ -f ${CACHE_DIR}/removeM2Cache ]"
 
   compile
 
   assertCapturedSuccess
-  assertTrue "removeM2Cache file should now exist in cache" "[ -f ${CACHE_DIR}/removeM2Cache ]"
-  assertFalse ".m2 should not be copied to build dir" "[ -d ${BUILD_DIR}/.m2 ]"
-  assertFalse ".maven should not be copied to build dir" "[ -d ${BUILD_DIR}/.maven ]"
-}
-
-testNonLegacyExistingAppsRemoveCache()
-{
-  createPom
-  touch ${CACHE_DIR}/removeM2Cache # simulate a previous run with no cache dir
-
-  assertTrue "Precondition: Existing apps should have a CACHE_DIR from previous run" "[ -d ${CACHE_DIR} ]"
-  assertTrue "Precondition: Existing apps should have a removeM2Cache file from previous run" "[ -f ${CACHE_DIR}/removeM2Cache ]"
-
-  compile
-
-  assertCapturedSuccess
-  assertTrue "removeM2Cache file should still exist in cache" "[ -f ${CACHE_DIR}/removeM2Cache ]"
   assertFalse ".m2 should not be copied to build dir" "[ -d ${BUILD_DIR}/.m2 ]"
   assertFalse ".maven should not be copied to build dir" "[ -d ${BUILD_DIR}/.maven ]"
 }
@@ -314,7 +296,7 @@ EOF
   assertNotCaptured "Installing Maven"
   assertFileMD5 "7d2bdb60388da32ba499f953389207fe" ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
-  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -file.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
+  assertCaptured "executing $CACHE_DIR/.maven/bin/mvn -B -Dfile.encoding=UTF-8 -Duser.home=$BUILD_DIR -Dmaven.repo.local=$CACHE_DIR/.m2/repository  -DskipTests=true clean install"
   assertCaptured "BUILD SUCCESS"
 }
 
