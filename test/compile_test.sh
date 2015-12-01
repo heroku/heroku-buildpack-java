f8a69ef7f05a3d86e11ff9cf37759d8ab5d651df
Joe Kutner <jpkutner@gmail.com>
2015-12-01 11:40:47 -0600

    Update mvn hash

--- test/compile_test.sh
+++ test/compile_test.sh
@@ -110,7 +110,7 @@ _assertMaven325() {
 
 _assertMavenLatest() {
   assertCaptured "Wrong Maven Installed" "Installing Maven 3.3.9"
-  assertFileMD5 "33b5239bdf488c9867f95dbb93ffc0a6" ${CACHE_DIR}/.maven/bin/mvn
+  assertFileMD5 "b34974f4c849ec2ae6481651e1f24ef1" ${CACHE_DIR}/.maven/bin/mvn
   assertTrue "mvn should be executable" "[ -x ${CACHE_DIR}/.maven/bin/mvn ]"
 
   assertCaptured "Executing: mvn -B -DskipTests clean dependency:list install"
