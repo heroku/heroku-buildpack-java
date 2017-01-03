0963b94b138b1bae473b056b2d2938ab59f293b6
Joe Kutner <jpkutner@gmail.com>
2017-01-03 11:32:43 -0600

    Improved formatting of mvn cmd opts

--- lib/maven.sh
+++ lib/maven.sh
@@ -18,10 +18,10 @@ _mvn_cmd_opts() {
   local scope=${1}
 
   if [ "$scope" = "compile" ]; then
-    echo -n " ${MAVEN_CUSTOM_OPTS:-"-DskipTests"}"
+    echo -n "${MAVEN_CUSTOM_OPTS:-"-DskipTests"}"
     echo -n " ${MAVEN_CUSTOM_GOALS:-"clean dependency:list install"}"
   elif [ "$scope" = "test-compile" ]; then
-    echo -n " ${MAVEN_CUSTOM_GOALS:-"clean dependency:resolve-plugins test-compile"}"
+    echo -n "${MAVEN_CUSTOM_GOALS:-"clean dependency:resolve-plugins test-compile"}"
   else
     echo -n ""
   fi
