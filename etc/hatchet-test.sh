ef7758238a67a02b7fcc1f6906cadd7a0895b500
Joe Kutner <jpkutner@gmail.com>
2017-02-07 10:03:32 -0600

    Added PR guard for hatchet tests

--- etc/hatchet-test.sh
+++ etc/hatchet-test.sh
@@ -2,10 +2,14 @@
 
 set -e
 
-export HEROKU_API_KEY="$(heroku auth:token)"
-export HATCHET_RETRIES=3
-export HATCHET_DEPLOY_STRATEGY=git
-export HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-java.git"
-export HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#')
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping Hatchet tests on Pull Request."
+else
+  export HEROKU_API_KEY="$(heroku auth:token)"
+  export HATCHET_RETRIES=3
+  export HATCHET_DEPLOY_STRATEGY=git
+  export HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-java.git"
+  export HATCHET_BUILDPACK_BRANCH=$(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#')
 
-./mvnw verify
+  ./mvnw verify
+fi
