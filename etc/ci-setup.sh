c7822988ebfebb3b117e9f6f64ff2b965dce63ac
Joe Kutner <jpkutner@gmail.com>
2017-06-20 10:08:18 -0700

    Only add keys on CI when logged in

--- etc/ci-setup.sh
+++ etc/ci-setup.sh
@@ -25,4 +25,6 @@ EOF
 
 curl --fail --retry 3 --retry-delay 1 --connect-timeout 3 --max-time 30 https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
-yes | heroku keys:add
+if [ -z "$HEROKU_API_KEY" ]; then
+  yes | heroku keys:add
+fi
