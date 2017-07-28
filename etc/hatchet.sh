102f616541278d1a104cf91982ad44efa0472465
Joe Kutner <jpkutner@gmail.com>
2017-07-28 13:47:30 -0500

    Better handling of 3rd party PRs with hatchet tests

--- etc/hatchet.sh
+++ etc/hatchet.sh
@@ -2,10 +2,28 @@
 
 set -e
 
+BUILDPACK_NAME="heroku-buildpack-java"
+
+if [ "$CIRCLECI" == "true" && -n "$CI_PULL_REQUEST" ]; then
+  if [ "$CIRCLE_PR_USERNAME" != "heroku" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
+
+if [ "$TRAVIS" == "true" && "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  if [ "$TRAVIS_PULL_REQUEST_SLUG" != "heroku/$BUILDPACK_NAME" ]; then
+    echo "Skipping integration tests on forked PR."
+    exit 0
+  fi
+fi
+
 if [ -z "$HEROKU_API_KEY" ]; then
-  echo "Missing \$HEROKU_API_KEY. Skipping."
-  exit 0
-elif [ -n "$CIRCLE_BRANCH" ]; then
+  echo "ERROR: Missing \$HEROKU_API_KEY."
+  exit 1
+fi
+
+if [ -n "$CIRCLE_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$CIRCLE_BRANCH"
 elif [ -n "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
   export HATCHET_BUILDPACK_BRANCH="$TRAVIS_PULL_REQUEST_BRANCH"
@@ -20,6 +38,6 @@ bundle exec hatchet install
 export HATCHET_RETRIES=3
 export HATCHET_APP_LIMIT=20
 export HATCHET_DEPLOY_STRATEGY=git
-export HATCHET_BUILDPACK_BASE="https://github.com/heroku/heroku-buildpack-java.git"
+export HATCHET_BUILDPACK_BASE="https://github.com/heroku/$BUILDPACK_NAME"
 
 bundle exec rspec $@
