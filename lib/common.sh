25e30a904745ccf7adef0cd528139cefa6787e67
Joe Kutner <jpkutner@gmail.com>
2019-09-27 09:44:16 -0500

    Add support for Maven 3.5 and 3.6

--- lib/common.sh
+++ lib/common.sh
@@ -14,8 +14,8 @@ install_maven() {
   mcount "mvn.version.${mavenVersion}"
 
   status_pending "Installing Maven ${mavenVersion}"
-  if is_supported_maven_version ${mavenVersion}; then
-    mavenUrl="https://lang-jvm.s3.amazonaws.com/maven-${mavenVersion}.tar.gz"
+  mavenUrl="https://lang-jvm.s3.amazonaws.com/maven-${mavenVersion}.tar.gz"
+  if is_supported_maven_version "${mavenVersion}" "${mavenUrl}"; then
     download_maven ${mavenUrl} ${installDir} ${mavenHome}
     status_done
   else
@@ -36,15 +36,10 @@ download_maven() {
 
 is_supported_maven_version() {
   local mavenVersion=${1}
+  local mavenUrl=${2}
   if [ "$mavenVersion" = "$DEFAULT_MAVEN_VERSION" ]; then
     return 0
-  elif [ "$mavenVersion" = "3.2.5" ]; then
-    return 0
-  elif [ "$mavenVersion" = "3.2.3" ]; then
-    return 0
-  elif [ "$mavenVersion" = "3.1.1" ]; then
-    return 0
-  elif [ "$mavenVersion" = "3.0.5" ]; then
+  elif curl -I --retry 3 --fail --silent --max-time 5 --location ${mavenUrl}; then
     return 0
   else
     return 1
