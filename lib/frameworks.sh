37f290e26827f60142f7638e2161a7062f340dcd
Manuel Fuchs <manuel.fuchs@salesforce.com>
2025-06-24 12:48:07 +0200

    Simplify frameworks.sh implementations

--- lib/frameworks.sh
+++ lib/frameworks.sh
@@ -6,35 +6,30 @@ set -euo pipefail
 
 frameworks::is_spring_boot() {
 	local build_dir="${1}"
-	[[ -f "${build_dir}/pom.xml" ]] &&
-		[[ -n "$(grep "<groupId>org.springframework.boot" "${build_dir}/pom.xml")" ]] &&
-		[[ -n "$(grep "<artifactId>spring-boot" "${build_dir}/pom.xml")" ]]
+
+	grep -qs "<groupId>org.springframework.boot" "${build_dir}/pom.xml" &&
+		grep -qs "<artifactId>spring-boot" "${build_dir}/pom.xml"
 }
 
 frameworks::is_wildfly_swarm() {
 	local build_dir="${1}"
-	[[ -f "${build_dir}/pom.xml" ]] &&
-		[[ -n "$(grep "<groupId>org.wildfly.swarm" "${build_dir}/pom.xml")" ]]
+	grep -qs "<groupId>org.wildfly.swarm" "${build_dir}/pom.xml"
 }
 
 frameworks::is_micronaut() {
 	local build_dir="${1}"
-	[[ -f "${build_dir}/pom.xml" ]] &&
-		[[ -n "$(grep "<groupId>io.micronaut" "${build_dir}/pom.xml")" ]]
+	grep -qs "<groupId>io.micronaut" "${build_dir}/pom.xml"
 }
 
 frameworks::is_quarkus() {
 	local build_dir="${1}"
-	[[ -f "${build_dir}/pom.xml" ]] &&
-		[[ -n "$(grep "<groupId>io.quarkus" "${build_dir}/pom.xml")" ]]
+	grep -qs "<groupId>io.quarkus" "${build_dir}/pom.xml"
 }
 
 frameworks::has_postgres() {
 	local build_dir="${1}"
-	# shellcheck disable=SC2235
-	[[ -f "${build_dir}/pom.xml" ]] && (
-		[[ -n "$(grep "<groupId>org.postgresql" "${build_dir}/pom.xml")" ]] ||
-			[[ -n "$(grep "<groupId>postgresql" "${build_dir}/pom.xml")" ]] ||
-			[[ -n "$(grep "<groupId>com.impossibl.pgjdbc-ng" "${build_dir}/pom.xml")" ]]
-	)
+
+	grep -qs "<groupId>org.postgresql" "${build_dir}/pom.xml" ||
+		grep -qs "<groupId>postgresql" "${build_dir}/pom.xml" ||
+		grep -qs "<groupId>com.impossibl.pgjdbc-ng" "${build_dir}/pom.xml"
 }
