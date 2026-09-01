f62a55d3c29d057ebadfaa2498ffa66ed9511dd0
Manuel Fuchs <manuel.fuchs@salesforce.com>
2026-09-01 13:14:38 +0200

    Strip literal '^[' ANSI escapes in test output cleaning

--- test/spec/spec_helper.rb
+++ test/spec/spec_helper.rb
@@ -75,7 +75,10 @@ def clean_output(output)
     # https://github.com/heroku/hatchet/issues/162
     .gsub(/ {8}(?=\R)/, '')
     # Remove ANSI colour codes used in buildpack output (e.g. error messages).
-    .gsub(/\e\[[0-9;]+m/, '')
+    # Some environments (notably GitHub Actions runners) deliver these with the
+    # escape byte rendered as the two-character literal "^[" instead of 0x1B,
+    # so strip both variants.
+    .gsub(/(?:\e|\^\[)\[[0-9;]+m/, '')
     # Remove trailing space from empty "remote: " lines added by Heroku
     .gsub(/^remote: $/, 'remote:')
 end
